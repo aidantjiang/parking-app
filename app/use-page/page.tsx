430592a206cec50aef303bc521744a5f7fbc2083
Aidan Jiang <85361427+aidantjiang@users.noreply.github.com>
2025-10-29 14:50:46 -0600

    all pages

---
+++ app/use-page/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function eventChoice() {
+  return (
+    <Link href="/create-new-app">
+        <button>hello</button>
+    </Link>
+  );
+}
