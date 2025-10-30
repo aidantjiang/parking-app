f5009ca66905e9158130d5b887c093dcacffbb0b
Aidan Jiang <85361427+aidantjiang@users.noreply.github.com>
2025-10-30 11:21:07 -0600

    hello

--- app/use-page/page.tsx
+++ app/use-page/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from 'next/link';
 
-export default function eventChoice() {
+export default function usePage() {
   return (
     <Link href="/create-new-app">
         <button>hello</button>
